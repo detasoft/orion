5a194319df7dbde9069ce998bcaeacff31116d44
Vladimir I <vladilm1v@gmail.com>
2026-09-11 10:51:56 +0200

    terminal-input-shutdown: Unblock terminal input on close

--- agentd/src/main/java/pro/deta/orion/agentd/terminal/PosixTerminal.java
+++ agentd/src/main/java/pro/deta/orion/agentd/terminal/PosixTerminal.java
@@ -1,12 +1,14 @@
 package pro.deta.orion.agentd.terminal;
 
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.nio.channels.Channels;
+import java.nio.channels.FileChannel;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.StandardOpenOption;
 import java.time.Duration;
 import java.util.ArrayList;
 import java.util.List;
@@ -191,7 +193,7 @@ final class PosixTerminal implements TerminalDevice {
         private final OutputStream output;
 
         private NativePlatform() throws IOException {
-            input = new FileInputStream(TTY);
+            input = Channels.newInputStream(FileChannel.open(TTY.toPath(), StandardOpenOption.READ));
             try {
                 output = new FileOutputStream(TTY);
             } catch (IOException failure) {
